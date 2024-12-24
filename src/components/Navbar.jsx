553707b660eb990b9993b2c17456402ef10037e8
ARYAN <aryan.70921@gmail.com>
2024-12-24 19:48:34 +0530

    ok

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
+
 
 const Navbar = () => {
   return (
     <div className='bg-black text-white flex justify-between gap-2'>
-        <div>Hamburger</div>
+        <div>Ham</div>
         <div>Search</div>
         <div>Login</div>
     </div>
   )
 }
 
-export default Navbar
+export default Navbar;
