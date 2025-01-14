afe4ce820f9e63169d1d6eaeacb8d0a540da9cc7
Ad-cmd-1976 <adityavishwakarma9025@gmail.com>
2025-01-14 15:37:23 +0530

    okay

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,8 +1,111 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import {ToastContainer} from 'react-toastify'
+import {handleError,handleSuccess} from '../util'
 const Signup = () => {
+  const [signInfo, setsignInfo] = useState({
+    name:'',
+    email:'',
+    password:''
+  })
+  const navigate=useNavigate();
+  const handleChange=(e)=>{
+    const {name,value}=e.target;
+    const cpysignInfo={...signInfo};
+    cpysignInfo[name]=value;
+    setsignInfo(cpysignInfo);
+  }
+  const handleSubmit=async (e)=>{
+    e.preventDefault();
+    const {name,email,password}=signInfo;
+    if(!name || !email || !password){
+      return handleError('name,email and password required!');
+    }
+    try{
+      const url='http://localhost:8080/auth/signup';
+      const res=await fetch(url,{
+        method:'POST',
+        headers:{
+          'Content-type':'application/json'
+        },
+        body:JSON.stringify(signInfo)
+      })
+      const result=await res.json();
+      console.log(result);
+      const {success,message,error}=result;
+      if(success){
+        handleSuccess(message);
+        setTimeout(() => {
+          navigate('/login');
+        }, 1000);
+      }
+      else if(error){
+        const detail=error?.details[0].message;
+        handleError(detail);
+      }
+      else if(!success){
+        handleError(message);
+      }
+    }catch(err){
+      handleError(err);
+    }
+  }
   return (
-    <div>Signup</div>
+    <div className='flex flex-col h-screen justify-center items-center'>
+      <h1>Sign Up</h1>
+      <form onSubmit={handleSubmit} action="" method="post">
+        <div>
+          <label htmlFor="name">Name</label>
+          <input
+            onChange={handleChange}
+            className='min-w-fit border-2 rounded-sm'
+            type='text'
+            id='name'
+            placeholder='Enter Your Name'
+            name='name'
+            value={signInfo.name}
+          />
+        </div>
+        <div>
+          <label htmlFor="name">Email</label>
+          <input
+            onChange={handleChange}
+            className='min-w-fit border-2 rounded-sm'
+            type='text'
+            id='email'
+            placeholder='Enter Your Email'
+            name='email'
+            value={signInfo.email}
+          />
+        </div>
+        <div>
+          <label htmlFor="name">Password</label>
+          <input
+            onChange={handleChange}
+            className='min-w-fit border-2 rounded-sm'
+            type='password'
+            id='password'
+            placeholder='Enter Your Password'
+            name='password'
+            value={signInfo.password}
+          />
+        </div>
+        <div>
+          <button
+          className='w-full border-2' 
+          type="submit">
+            Sign Up
+          </button>
+        </div>
+        <span>
+          Already have an account?{" "}
+          <Link to='/login' className='text-blue-600 hover:underline'>
+          Login
+          </Link>
+        </span>
+      </form>
+      <ToastContainer/>
+    </div>
   )
 }
 
--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,8 +1,99 @@
-import React from 'react'
-
+import {useState} from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import {handleError,handleSuccess} from '../util'
 const Login = () => {
+  const [loginInfo, setloginInfo] = useState({
+    email:'',
+    password:''
+  })
+  const navigate=useNavigate();
+  const handleChange=(e)=>{
+    const {name,value}=e.target;
+    const cpyloginInfo={...loginInfo};
+    cpyloginInfo[name]=value;
+    setloginInfo(cpyloginInfo);
+  }
+  const handlelogin=async (e)=>{
+    e.preventDefault();
+    const {email,password}=loginInfo;
+    if(!email || !password){
+      handleError('email and password are required!');
+    }
+    try{
+      const url='http://localhost:8080/auth/login';
+      const res=await fetch(url,{
+        method:'POST',
+        headers:{
+          'Content-type':'application/json'
+        },
+        body:JSON.stringify(loginInfo)
+      })
+      const result=await res.json();
+      console.log(result);
+      const {success,message,error,jwtToken,name}=result;
+      if(success){
+        handleSuccess(message);
+        localStorage.setItem('token',jwtToken);
+        localStorage.setItem('name',name);
+        setTimeout(() => {
+          navigate('/');
+        }, 1000);
+      }
+      else if(error){
+        const detail=error?.details[0].message;
+        handleError(detail);
+      }
+      else if(!success){
+        handleError(message);
+      }
+    }catch(err){
+      handleError(err);
+    }
+  }
   return (
-    <div>login</div>
+    <div 
+    className='flex flex-col justify-center items-center h-screen'
+    >
+      <h1>Login</h1>
+      <form onSubmit={handlelogin} method="post">
+        <div>
+          <label htmlFor="email">Email</label>
+          <input 
+          onChange={handleChange}
+          type="text" 
+          name="email" 
+          id="email" 
+          value={loginInfo.email}
+          className='border-2 min-w-fit'
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input 
+          onChange={handleChange}
+          type="password" 
+          name="password" 
+          id="password" 
+          value={loginInfo.password}
+          className='border-2 min-w-fit'
+          />
+        </div>
+        <div>
+          <button 
+          type="submit"
+          className='w-full border-2'
+          >
+            Login
+          </button>
+        </div>
+        <span>
+          Don't have an account {" "}
+          <Link to='/sigup' className='text-blue-600 hover:underline'>Signup</Link>
+        </span>
+      </form>
+      <ToastContainer />
+    </div>
   )
 }
 
